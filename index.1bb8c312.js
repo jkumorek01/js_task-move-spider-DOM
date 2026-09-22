351bfe43c312220ae3616026b64665471b13b284
jkumorek01 <jkumorek01@gmail.com>
2026-09-22 14:31:04 +0200

    make build

---
+++ index.1bb8c312.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall"),c=t.getBoundingClientRect(".wall");t.addEventListener("click",function(t){var l=t.clientX-c.left,n=t.clientY-c.top;e.style.top="".concat(n,"px"),e.style.left="".concat(l,"px")});
+//# sourceMappingURL=index.1bb8c312.js.map
