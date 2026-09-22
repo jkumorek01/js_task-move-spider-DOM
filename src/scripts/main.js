82f859b81862fb8155e91e9ed7bda525f2fbc8d6
jkumorek01 <jkumorek01@gmail.com>
2026-09-22 14:30:41 +0200

    First version of working spider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,16 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const spider = document.querySelector('.spider');
+const wall = document.querySelector('.wall');
+const wallSize = wall.getBoundingClientRect('.wall');
+
+// console.log(wallSize);
+
+wall.addEventListener('click', (e) => {
+  const x = e.clientX - wallSize.left;
+
+  const y = e.clientY - wallSize.top;
+
+  spider.style.top = `${y}px`;
+  spider.style.left = `${x}px`;
 });
